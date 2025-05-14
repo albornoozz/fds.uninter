8a014a765d1eb31cd82ac298f9abc107f885f642
albornoozz <albornoozdigital@gmail.com>
2025-05-14 00:21:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("#botao").addEventListener("click", function() {
+    document.querySelector("#mensagem").innerHTML= "<strong>Seja bem-vindo(a) ao meu mundo!</strong>";
+});
+
